81bca77e93061811181a9903cda0a49121e94293
Dru Serkes <DruSerkes@gmail.com>
2020-10-12 00:26:24 -0400

    button click dispatches store to get questions

--- src/components/Start.tsx
+++ src/components/Start.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import * as React from 'react';
+import { useDispatch } from 'react-redux';
+import { getQuestions } from '../redux/actions';
 
 const Start = () => {
-
+    const dispatch = useDispatch();
+    
+    return (
+        <div className="Start">
+            <button onClick={() => dispatch(getQuestions())}>Start Game</button>
+        </div>
+    )
 }
 
 export default Start;
