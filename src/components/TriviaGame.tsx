1aab86662033974e0e9f3880088b623ea63c6be8
Dru Serkes <DruSerkes@gmail.com>
2020-10-12 01:08:28 -0400

    import Result component

--- src/components/TriviaGame.tsx
+++ src/components/TriviaGame.tsx
@@ -1,25 +1,26 @@
 /*
-    * Redux lives here 
-    * Local state to keep track of current question (which gets passed down) 
-    * Local state to keep track of answers (correct, answered)  
-    * Renders Start if no questions in state
-    * Renders QuizCard otherwise
-        * pass question data down 
-    * Renders Result if …
-    * 
-    * 
-    * res.data.results[0-9]
-        * set amount=10
-        * set type=multiple
-        * .question 
-        * .correct_answer 
-        * .incorrect_answers[0-2]
+	* Redux lives here 
+	* Local state to keep track of current question (which gets passed down) 
+	* Local state to keep track of answers (correct, answered)  
+	* Renders Start if no questions in state
+	* Renders QuizCard otherwise
+		* pass question data down 
+	* Renders Result if …
+	* 
+	* 
+	* res.data.results[0-9]
+		* set amount=10
+		* set type=multiple
+		* .question 
+		* .correct_answer 
+		* .incorrect_answers[0-2]
 */
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { StoreType } from '../redux/store';
 import { QuestionType } from '../redux/types';
-import {Start} from './Start';
+import { Start } from './Start';
+import { Result } from './Result';
 
 // TODO
 // 2. Create QuizCard component
@@ -28,9 +29,9 @@ import {Start} from './Start';
 
 const TriviaGame: React.FC = () => {
 	const questions = useSelector((state: StoreType) => state.questions);
-	const [ currentQuestion, setCurrentQuestion ] = useState<QuestionType | null>(null);
-	const [ numAnswered, setNumAnswered ] = useState(0);
-	const [ numCorrect, setNumCorrect ] = useState(0);
+	const [currentQuestion, setCurrentQuestion] = useState<QuestionType | null>(null);
+	const [numAnswered, setNumAnswered] = useState(0);
+	const [numCorrect, setNumCorrect] = useState(0);
 
 	const answerQuestion = (answer: string) => {
 		if (currentQuestion && answer === currentQuestion.correct_answer) setNumCorrect((correct) => correct++);
@@ -51,8 +52,8 @@ const TriviaGame: React.FC = () => {
 			{numAnswered < 10 ? (
 				<QuizCard question={currentQuestion} answerQuestion={answerQuestion} />
 			) : (
-				<Result numCorrect={numCorrect} />
-			)}
+					<Result numCorrect={numCorrect} />
+				)}
 		</div>
 	);
 };
