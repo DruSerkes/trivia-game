f6ef8cabadfaca097a185c11b410c688e0e0ab4b
Dru Serkes <DruSerkes@gmail.com>
2020-10-12 03:16:53 -0400

    add TODOs

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import './App.css';
 import { TriviaGame } from './components/TriviaGame';
 
-
+/* TODO 
+1. Refactor with MUI components 
+2. Add custom styles where necessary 
+3. Add next features 
+*/
 function App() {
       return (
             <div className="App">
