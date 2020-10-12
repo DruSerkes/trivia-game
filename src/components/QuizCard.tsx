4cd795f7a6f7a3fcef6cbadad50a7d880de76c0a
Dru Serkes <DruSerkes@gmail.com>
2020-10-12 01:26:38 -0400

    create QuizCard with TODOs

---
+++ src/components/QuizCard.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { useState } from 'react';
+import { QuestionType } from '../redux/types';
+
+type QuizCardProps = {
+    question: QuestionType;
+    answerQuestion: (answer: string) => void
+}
+
+
+export const QuizCard = ({ question, answerQuestion }: QuizCardProps) => {
+    // Render the 3 incorrect answers and the correct answer
+    // Do so with each being a separate Answer component
+    // Answer component should accept props for question and isCorrect (optional bool)
+    // When Answer is clicked, it should fire off answerQuestion with the answer passed in
+
+
+    return (
+        <div className="QuizCard">
+            <h4>{question.question}</h4>
+            {/* RENDER 4 ANSWERS HERE */}
+        </div>
+    )
+}
