7b4cec50917f0377af04cc23f481e828d0a6a35d
Dru Serkes <DruSerkes@gmail.com>
2020-10-12 02:04:04 -0400

    format JSX

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import './App.css';
 import TriviaGame from './components/TriviaGame';
 
 function App() {
-      return <div className="App">
-            <TriviaGame />
-      </div>;
+      return (
+            <div className="App">
+                  <TriviaGame />
+            </div>);
 }
 
 export default App;
