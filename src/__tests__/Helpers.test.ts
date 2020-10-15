f5178cc75423a23e68f79237acb163b9b8d77966
Dru Serkes <DruSerkes@gmail.com>
2020-10-15 12:21:02 -0400

    test helpers

---
+++ src/__tests__/Helpers.test.ts
@@ -0,0 +1,19 @@
+import { renderColor } from '../helpers/renderColor'
+import { shuffle } from '../helpers/shuffle'
+
+
+describe('helpers tests', () => {
+
+    test('shuffle', () => {
+        let arrayToShuffle = ['hi', 'hello', 'testing', '123', 'leggo my eggo']
+        expect(shuffle(arrayToShuffle)).not.toEqual(arrayToShuffle)
+    })
+
+    test('renderColor', () => {
+        expect(renderColor(2)).toEqual('red');
+        expect(renderColor(4)).toEqual('orange');
+        expect(renderColor(6)).toEqual('yellow');
+        expect(renderColor(8)).toEqual('green');
+        expect(renderColor(10)).toEqual('blue')
+    })
+})
