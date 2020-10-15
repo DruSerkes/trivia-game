0147c84b641a27b80a13e81d37fd339a70767e6c
Dru Serkes <DruSerkes@gmail.com>
2020-10-15 12:03:26 -0400

    add gutterBottom

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,12 @@ import { TriviaGame } from './components/TriviaGame';
 import { Typography, Box } from '@material-ui/core'
 
 /* TODO 
-1. Refactor with MUI components 
-2. Add custom styles where necessary 
 3. Add next features 
 */
 function App() {
       return (
             <Box className="App">
-                  <Typography variant="h1">Trivia Game!</Typography>
+                  <Typography gutterBottom variant="h1">Trivia Game!</Typography>
                   <TriviaGame />
             </Box>);
 }
