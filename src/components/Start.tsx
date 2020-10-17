94d43be1155e39b82f8835ae68698fd891efec5c
Dru Serkes <DruSerkes@gmail.com>
2020-10-17 18:38:17 -0400

    remove TODOs and comments

--- src/components/Start.tsx
+++ src/components/Start.tsx
@@ -5,13 +5,7 @@ import { Box, Button, TextField, FormControl, RadioGroup, FormControlLabel, Radi
 type StartProps = {
     startGame: (amount: string, difficulty: string) => void
 }
-/*
-1. Add state for difficulty ✅
-2. Add a field for difficulty: easy, medium, hard, surprise me! ✅
-3. check that state works as expected 
-4. Update startGame to include difficulty from user choice 
-5. check response 
-*/
+
 export const Start = ({ startGame }: StartProps) => {
     const [amount, setAmount] = useState<string>('10');
     const [difficulty, setDifficulty] = useState<string>('');
@@ -19,7 +13,6 @@ export const Start = ({ startGame }: StartProps) => {
     const handleChangeAmount = (e: React.ChangeEvent<HTMLInputElement>): void => setAmount(e.target.value)
     const handleChangeDifficulty = (e: React.ChangeEvent<HTMLInputElement>): void => setDifficulty(e.target.value)
     const handleSubmit = () => {
-        // console.log(amount, difficulty);
         startGame(amount, difficulty)
     }
     return (
