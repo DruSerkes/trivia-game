a82331e50603870c3c490ab89d4c41b5a66f49b1
Dru Serkes <DruSerkes@gmail.com>
2020-10-18 16:15:57 -0400

    update result tests

--- src/__tests__/Result.test.tsx
+++ src/__tests__/Result.test.tsx
@@ -5,7 +5,19 @@ import { Result } from '../components/Result'
 
 test('Result Matches Snapshot ', () => {
     const { asFragment } = render(
-        <Result numCorrect={666} />
+        <Result numCorrect={666} numAnswered={667} />
     );
     expect(asFragment()).toMatchSnapshot();
+});
+
+
+
+test('Result renders expected text ', () => {
+    let numCorrect = 5
+    let numAnswered = 10
+    const { getByText } = render(
+        <Result numCorrect={numCorrect} numAnswered={numAnswered} />
+    );
+    const resultText = getByText(`You answered ${numCorrect} out of ${numAnswered} correct`)
+    expect(resultText).toBeInTheDocument();
 });
