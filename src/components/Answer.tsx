6506c2943fd2734073d80299399a3d0c2328aa3b
Dru Serkes <DruSerkes@gmail.com>
2020-10-18 17:08:40 -0400

    update AnswerProps and types

--- src/components/Answer.tsx
+++ src/components/Answer.tsx
@@ -3,12 +3,20 @@ import { ListItem } from '@material-ui/core'
 
 type AnswerProps = {
     answer: string;
-    answerQuestion: (answer: string) => void
+    answerQuestion: (answer: string) => void;
+    correct: boolean | null;
 }
 
-export function Answer({ answer, answerQuestion }: AnswerProps) {
+export function Answer({ answer, answerQuestion, correct }: AnswerProps) {
     const renderHTML = () => (<span className="AnswerText" dangerouslySetInnerHTML={{ __html: answer }} />)
+    handleClick = (e: React.MouseEvent<HTMLLIElement>) => {
+        console.log(e.target);
+        // document.getElementById(`${answer}`)
+        // if (correct) e.target.push()
+    }
     return (
+        // <ListItem id={answer} className="Answer" divider onClick={handleClick}>{renderHTML()}</ListItem>
+
         <ListItem className="Answer" divider onClick={() => answerQuestion(answer)}>{renderHTML()}</ListItem>
     );
 }
