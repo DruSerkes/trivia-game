39250222cea78baa75924c8f4678e5631e8a8bad
Dru Serkes <DruSerkes@gmail.com>
2020-10-20 10:47:55 -0400

    add radio field for type

--- src/components/Start.tsx
+++ src/components/Start.tsx
@@ -9,10 +9,12 @@ type StartProps = {
 export const Start = ({ startGame }: StartProps) => {
     const [amount, setAmount] = useState<string>('10');
     const [difficulty, setDifficulty] = useState<string>('');
+    const [type, setType] = useState<string>('multiple');
 
-    const handleChangeAmount = (e: React.ChangeEvent<HTMLInputElement>): void => setAmount(e.target.value)
-    const handleChangeDifficulty = (e: React.ChangeEvent<HTMLInputElement>): void => setDifficulty(e.target.value)
-    const handleSubmit = () => startGame(amount, difficulty)
+    const handleChangeAmount = (e: React.ChangeEvent<HTMLInputElement>): void => setAmount(e.target.value);
+    const handleChangeDifficulty = (e: React.ChangeEvent<HTMLInputElement>): void => setDifficulty(e.target.value);
+    const handleChangeType = (e: React.ChangeEvent<HTMLInputElement>): void => setType(e.target.value);
+    const handleSubmit = () => startGame(amount, difficulty, type)
 
     return (
         <Box className="Start">
@@ -24,6 +26,13 @@ export const Start = ({ startGame }: StartProps) => {
                     <FormControlLabel value="medium" control={<Radio />} label="Medium" labelPlacement="end" />
                     <FormControlLabel value="hard" control={<Radio />} label="Hard" labelPlacement="end" />
                     <FormControlLabel value="" control={<Radio />} label="Surprise Me!" labelPlacement="end" />
+                </RadioGroup>
+            </FormControl>
+            <FormControl component="fieldset">
+                <FormLabel component="legend">Type</FormLabel>
+                <RadioGroup row aria-label="type" name="type" value={type} onChange={handleChangeType}>
+                    <FormControlLabel value="multiple" control={<Radio />} label="Multiple Choice" labelPlacement="end" />
+                    <FormControlLabel value="boolean" control={<Radio />} label="True/False" labelPlacement="end" />
                 </RadioGroup>
             </FormControl>
             <Button variant="contained" fullWidth color="primary" onClick={handleSubmit}>Start New Game</Button>
