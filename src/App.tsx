cb0c9e88e43af5f2a1ebce06ecb34f63d8cc651d
Dru Serkes <DruSerkes@gmail.com>
2020-10-24 10:45:46 -0400

    remove TODO

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { TriviaGame } from './components/TriviaGame';
 import { Typography, Box } from '@material-ui/core'
 
 /* TODO 
-1a. Debug background color staying the same when you click for True/False questions 
 2. Add feedback text for correct / incorrect 
 3. Refactor to show which was correct if incorrect was chosen... (give it a 1px green border)
 */
