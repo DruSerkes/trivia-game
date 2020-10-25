848b76cfff8884ffb68c2bd66a9e2df1871f0670
Dru Serkes <DruSerkes@gmail.com>
2020-10-25 11:38:01 -0400

    Updates

---
+++ precache-manifest.1f15f3c2eddafe1fd37c4f091d9b698d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "946e5fabb516ee09aef3362c0ed9f421",
+    "url": "/trivia-game/index.html"
+  },
+  {
+    "revision": "3d6d04fa3d4095a9b476",
+    "url": "/trivia-game/static/css/main.5f0c8cb1.chunk.css"
+  },
+  {
+    "revision": "624dfea3a73ac4ccd448",
+    "url": "/trivia-game/static/js/2.381a819f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/trivia-game/static/js/2.381a819f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d6d04fa3d4095a9b476",
+    "url": "/trivia-game/static/js/main.c1f85d02.chunk.js"
+  },
+  {
+    "revision": "ab88edfc4d4159ff6791",
+    "url": "/trivia-game/static/js/runtime-main.9fb55eb6.js"
+  }
+]);
