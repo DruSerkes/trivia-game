e4d3f0ac08ea8c2c006eb29d824bcddb39cf9a3f
Dru Serkes <DruSerkes@gmail.com>
2020-10-25 11:48:37 -0400

    update copy

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,11 @@ import './App.css';
 import { TriviaGame } from './components/TriviaGame';
 import { Typography, Box } from '@material-ui/core'
 
-/* TODO 
-*/
+
 function App() {
       return (
             <Box className="App">
-                  <Typography gutterBottom variant="h1">Trivia!</Typography>
+                  <Typography gutterBottom variant="h1">Lets Trivia!</Typography>
                   <TriviaGame />
             </Box>);
 }
