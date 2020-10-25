8178de9bd458e900eabd164dbb7c402ab9a635f8
Dru Serkes <DruSerkes@gmail.com>
2020-10-25 11:42:37 -0400

    add play again

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -12,6 +12,7 @@ export const Result = ({ numCorrect, numAnswered }: ResultTypes) => {
         <Box className="Result">
             <Typography variant="h3">Game Over!</Typography>
             <Typography variant="body1" style={{ color: renderColor(numCorrect, numAnswered) }}>You answered {numCorrect} out of {numAnswered} correct</Typography>
+            <Typography variant="h5">Play Again?</Typography>
         </Box>
     )
 }
