4c6a7ec1ca915422622ed53258d2f74bad6c3a53
Dru Serkes <DruSerkes@gmail.com>
2025-03-16 15:18:56 -0400

    Add animation and show the current year in the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React from "react";
 
 export const Footer = () => {
-    return (
-        <footer className="Footer">
-            <p>
-                <a data-testid="footer" href="https://github.com/druserkes" target="_blank" rel="noopener noreferrer">
-                    Dru Serkes &copy;2020
-				</a>
-            </p>
-        </footer>
-    );
+  const year = new Date().getFullYear();
+  return (
+    <footer className="Footer">
+      <p>
+        <a
+          data-testid="footer"
+          href="https://github.com/druserkes"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Dru Serkes &copy;{year}
+        </a>
+      </p>
+    </footer>
+  );
 };
